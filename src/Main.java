bf8bbe32ca402943d115d2417c400f98555d4c28
Alessandro <alessandrorosa770@gmail.com>
2022-10-31 16:56:24 +0100

    run lambda and learn the difference with anonymous classes

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,8 @@
 import java.util.Comparator;
+import java.util.function.Consumer;
 
 public class Main {
     public static void main(String[] args) {
-
-        /*
-         * All this four Comparators make identical results
-         */
-
         // Anonymus class
         Comparator<Employee> byName = new Comparator<Employee>() {
             @Override
@@ -19,11 +15,18 @@ public class Main {
         Comparator<Employee> byNameLambda1 = (Employee a, Employee b) -> {return a.getName().compareTo(b.getName());};
         Comparator<Employee> byNameLambda2 = (a, b) -> {return a.getName().compareTo(b.getName());};
         Comparator<Employee> byNameLambda3 = (a, b) -> a.getName().compareTo(b.getName());
-
+        int compare1 = byNameLambda3.compare(new Employee("gino", 1), new Employee("romeo", 2));
+        System.out.println(compare1);
 
         // Lambads without parameters
         Runnable r = () -> { System.out.println("A compact Runnable");};
         Thread t1 = new Thread(r);
 
+        // Lambdas use only one object Consumer<String> ( Anonymus classes use 5 onces in this example)
+        System.out.println("\nNon-capturing lambda:");
+        for (int i=0; i<5; i++) {
+            Consumer<String> myPrinter= msg -> System.out.println("Consuming " + msg);
+            myPrinter.accept(myPrinter.toString());
+        }
     }
 }
